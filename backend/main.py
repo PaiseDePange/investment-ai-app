0990ab2881c4370438495cfc81a8d103153cbc59
PaiseDePange <PaiseDePange@gmail.com>
2025-07-20 01:49:09 +0100

    Enable CORS for frontend to access backend

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 from routers import dcf, sensitivity
 from fastapi.middleware.cors import CORSMiddleware
 
+
 app = FastAPI() 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
